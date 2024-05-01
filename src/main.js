e87fbf41b890ecae8976f807239e3ff3106e89f3
mitchcamza <192mitch88@gmail.com>
2024-04-30 17:13:11 -0700

    Initial project setup: initialize scene; add solar system group with sun mesh

---
+++ src/main.js
@@ -0,0 +1,68 @@
+import * as THREE from 'three';
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+const aspect = { width: window.innerWidth, height: window.innerHeight };
+
+// Scene
+const scene = new THREE.Scene();
+
+// Camera
+const camera = new THREE.PerspectiveCamera(45, aspect.width / aspect.height, 0.1, 1000
+);
+camera.position.set(0, 0, 10);
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
+renderer.setSize(aspect.width, aspect.height);
+
+// Handle windows resize for responsiveness
+window.addEventListener('resize', () =>
+{
+    // Update the aspect ratio on resize
+    aspect.width = window.innerWidth;
+    aspect.height = window.innerHeight;
+
+    // Update the camera
+    camera.aspect = aspect.width / aspect.height;
+    camera.updateProjectionMatrix();
+
+    // Update the renderer
+    renderer.setSize(aspect.width, aspect.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    renderer.render(scene, camera);
+});
+
+// Handle fullscreen
+window.addEventListener('dblclick', () =>
+{
+    const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement;
+
+    if (!fullscreenElement)
+    {
+        canvas.requestFullscreen();
+    }
+    else
+    {
+        if (document.exitFullscreen)
+        {
+            document.exitFullscreen();
+        }
+        else if (document.webkitFullscreen)
+        {
+            document.webkitExitFullscreen();
+        }
+    }
+});
+
+// Sun
+const sunGeometry = new THREE.SphereGeometry(1, 32, 32);
+const sunMaterial = new THREE.MeshBasicMaterial({ color: 'yellow' });
+const sunMesh = new THREE.Mesh(sunGeometry, sunMaterial);
+const solarSystem = new THREE.Group();
+solarSystem.add(sunMesh);
+scene.add(solarSystem);
+
+// Render the scene
+renderer.render(scene, camera);
